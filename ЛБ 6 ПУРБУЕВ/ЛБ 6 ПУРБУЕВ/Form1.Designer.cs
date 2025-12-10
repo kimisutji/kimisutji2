2d440ada7576b85748ff0141f29cf7c83e91e5dd
kimisutji <purbuevallar@gmail.com>
2025-12-10 17:38:31 +0800

    Add files via upload

---
+++ ЛБ 6 ПУРБУЕВ/ЛБ 6 ПУРБУЕВ/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ЛБ_6_ПУРБУЕВ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.txtCols = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(31, 28);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(100, 20);
+            this.txtRows.TabIndex = 0;
+            this.txtRows.TextChanged += new System.EventHandler(this.txtRows_TextChanged);
+            // 
+            // txtCols
+            // 
+            this.txtCols.Location = new System.Drawing.Point(31, 68);
+            this.txtCols.Name = "txtCols";
+            this.txtCols.Size = new System.Drawing.Size(100, 20);
+            this.txtCols.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "кол-во строк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "кол-во столбцов";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(31, 113);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(127, 23);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Создать матрицу";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(31, 151);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(127, 23);
+            this.btnA.TabIndex = 5;
+            this.btnA.Text = "Задание А";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(31, 180);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(127, 23);
+            this.btnB.TabIndex = 6;
+            this.btnB.Text = "Задание В";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(31, 211);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(127, 23);
+            this.btnC.TabIndex = 7;
+            this.btnC.Text = "Задание С";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(177, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(347, 270);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(70, 13);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Результаты ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCols);
+            this.Controls.Add(this.txtRows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.TextBox txtCols;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ЛБ 6 ПУРБУЕВ/ЛБ 6 ПУРБУЕВ/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛБ_6_ПУРБУЕВ
+{
+    public partial class Form1 : Form
+    {
+        int[,] matrix;
+        int rows, cols;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // -------------------------
+        // СОЗДАНИЕ МАТРИЦЫ
+        // -------------------------
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtRows.Text, out rows) || rows <= 0 ||
+                !int.TryParse(txtCols.Text, out cols) || cols <= 0)
+            {
+                MessageBox.Show("Введите корректные размеры матрицы!");
+                return;
+            }
+
+            matrix = new int[rows, cols];
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Создаём столбцы
+            for (int j = 0; j < cols; j++)
+                dataGridView1.Columns.Add("c" + j, "C" + j);
+
+            // Заполняем строки
+            Random rnd = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                dataGridView1.Rows.Add(); // создаём пустую строку
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rnd.Next(-20, 21);
+                    dataGridView1[j, i].Value = matrix[i, j]; // выводим поэлементно
+                }
+            }
+
+            lblResult.Text = "Матрица создана!";
+        }
+
+        // Проверка
+        private bool CheckMatrix()
+        {
+            if (matrix == null)
+            {
+                MessageBox.Show("Сначала создайте матрицу!");
+                return false;
+            }
+            return true;
+        }
+
+        // -------------------------
+        // A) Произведение положительных кратных 3
+        // -------------------------
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            if (!CheckMatrix()) return;
+
+            int product = 1;
+            bool found = false;
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    if (matrix[i, j] > 0 && matrix[i, j] % 3 == 0)
+                    {
+                        product *= matrix[i, j];
+                        found = true;
+                    }
+
+            lblResult.Text = found
+                ? $"Произведение положительных кратных 3: {product}"
+                : "Подходящих элементов нет.";
+        }
+
+        // -------------------------
+        // B) Количество отрицательных на побочной диагонали
+        // побочная диагональ — индексы: (i, cols - i - 1)
+        // -------------------------
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            if (!CheckMatrix()) return;
+
+            int count = 0;
+
+            int diagLen = Math.Min(rows, cols); // на случай не квадратной матрицы
+
+            for (int i = 0; i < diagLen; i++)
+            {
+                int j = cols - i - 1;
+                if (matrix[i, j] < 0) count++;
+            }
+
+            lblResult.Text = $"Отрицательных на побочной диагонали: {count}";
+        }
+
+        private void txtRows_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // -------------------------
+        // C) В матрице 6x6:
+        // произведение (суммы чётных чисел 3 строки)
+        //         × (суммы отрицательных чисел 1 столбца)
+        // -------------------------
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            if (!CheckMatrix()) return;
+
+            if (rows != 6 || cols != 6)
+            {
+                MessageBox.Show("Это задание работает только для матрицы 6×6!");
+                return;
+            }
+
+            // 1) Сумма чётных чисел 3-й строки (индекс = 2)
+            int sumEven = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                if (matrix[2, j] % 2 == 0)
+                    sumEven += matrix[2, j];
+            }
+
+            // 2) Сумма отрицательных чисел первого столбца
+            int sumNeg = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                if (matrix[i, 0] < 0)
+                    sumNeg += matrix[i, 0];
+            }
+
+            int result = sumEven * sumNeg;
+
+            lblResult.Text = $"Результат задания C: {result}";
+        }
+    }
+}
+
